5f1a741ec57738b1f38274ec227c4b031301486e
Thushara <62303832+thushmanoj@users.noreply.github.com>
2025-09-11 20:01:17 +0530

    Update Color_pair.hpp

--- Color_pair.hpp
+++ Color_pair.hpp
@@ -1,4 +1,21 @@
 #ifndef COLOR_PAIR_HPP
 #define COLOR_PAIR_HPP
 
+namespace TelCoColorCoder{
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+    int numberOfMajorColors;
+    int numberOfMinorColors;
+   class ColorPair {
+        private:
+            MajorColor majorColor;
+            MinorColor minorColor;
+        public:
+            ColorPair(MajorColor major, MinorColor minor);
+            MajorColor getMajor();
+            MinorColor getMinor();
+            std::string ToString();
+    };
+}
+
 #endif
