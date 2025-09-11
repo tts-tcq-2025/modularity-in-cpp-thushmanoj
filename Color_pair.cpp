d76884f3ef455426ca49a12696463ed33fba0d2a
Thushara <62303832+thushmanoj@users.noreply.github.com>
2025-09-11 20:27:50 +0530

    Update Color_pair.cpp

--- Color_pair.cpp
+++ Color_pair.cpp
@@ -28,5 +28,15 @@ namespace TelCoColorCoder
                 colorPairStr += MinorColorNames[minorColor];
                 return colorPairStr;
             }
-    };
+   ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = 
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+    }
 }
