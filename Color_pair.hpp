7fce6739ad23ab653df27e862a61c8798c95521a
Thushara <62303832+thushmanoj@users.noreply.github.com>
2025-09-11 21:10:27 +0530

    Update Color_pair.hpp

--- Color_pair.hpp
+++ Color_pair.hpp
@@ -4,6 +4,12 @@
 namespace TelCoColorCoder{
     enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
     enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+    const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
+    const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
     int numberOfMajorColors;
     int numberOfMinorColors;
    class ColorPair {
