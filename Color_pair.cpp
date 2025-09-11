57750f5f52cfb86fb16d79d2c30219ee424cc2ff
Thushara <62303832+thushmanoj@users.noreply.github.com>
2025-09-11 20:23:26 +0530

    Update Color_pair.cpp

--- Color_pair.cpp
+++ Color_pair.cpp
@@ -1,1 +1,32 @@
 #include "Color_pair.cpp"
+
+namespace TelCoColorCoder
+{
+    const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
+    numberOfMajorColors =
+        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+    numberOfMinorColors =
+        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+    ColorPair :: ColorPair(MajorColor major, MinorColor minor):
+                majorColor(major), minorColor(minor)
+            {}
+    MajorColor ColorPair::getMajor() {
+                return majorColor;
+            }
+    MinorColor ColorPair::getMinor() {
+                return minorColor;
+            }
+    std::string ColorPair::ToString() {
+                std::string colorPairStr = MajorColorNames[majorColor];
+                colorPairStr += " ";
+                colorPairStr += MinorColorNames[minorColor];
+                return colorPairStr;
+            }
+    };
+}
